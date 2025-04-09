01be020a032aaa0e300d36c6c3aba4f7d38b522e
pumpk <cong2738@gmail.com>
2025-04-09 09:14:52 +0900

    doc

--- my_RISC-V.srcs/sources_1/new/rom.sv
+++ my_RISC-V.srcs/sources_1/new/rom.sv
@@ -11,6 +11,4 @@ module rom (
         rom[1] = 32'b0100000_00001_00010_000_00101_0110011;  //add x4, x2, x1
     end
     assign data = rom[addr[31:2]];//0 4 8 16을 0,1,2,3으로 바꿔주기 위해 2비트 시프트
-
-
 endmodule
