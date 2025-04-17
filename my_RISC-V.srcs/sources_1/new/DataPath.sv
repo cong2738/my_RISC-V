bd26dffdbb137d906f17ab6b52da4722ba04e62b
pumpk <cong2738@gmail.com>
2025-04-17 16:03:11 +0900

    feat: multi cycle core logic

--- my_RISC-V.srcs/sources_1/new/DataPath.sv
+++ my_RISC-V.srcs/sources_1/new/DataPath.sv
@@ -13,6 +13,7 @@ module DataPath (
     input  logic        branch,
     input  logic        jal,
     input  logic        jalr,
+    input  logic        PCEn,
     // instr memory side port
     output logic [31:0] instrMemAddr,
     input  logic [31:0] instrCode,
@@ -26,10 +27,13 @@ module DataPath (
     logic [31:0] immExt, aluSrcMuxOut, RFWDSrcMuxOut;
     logic btaken, PCSrcMuxSel;
     logic [31:0] PC_Imm_AdderResult, PC_4_AdderResult, PCSrcMuxOut;
-
+    logic [31:0] DecReg_RFData1, DecReg_RFData2, DecReg_ImmExt;
+    logic [31:0] ExeReg_aluResult, ExeReg_RFData2, ExeReg_PCSrcMuxOut;
+    logic [31:0] MemAccReg_dataRData;
+    
     assign instrMemAddr = PCOutData;
-    assign dataAddr     = aluResult;
-    assign dataWData    = RFData2;
+    assign dataAddr     = ExeReg_aluResult;
+    assign dataWData    = ExeReg_RFData2;
     assign PCSrcMuxSel  = jal | (btaken & branch);
 
     RegisterFile U_RegFile (
@@ -43,18 +47,47 @@ module DataPath (
         .RData2(RFData2)
     );
 
+
+    register U_DecReg_RFRD1 (
+        .clk  (clk),
+        .reset(reset),
+        .d    (RFData1),
+        .q    (DecReg_RFData1)
+    );
+
+    register U_DecReg_RFRD2 (
+        .clk  (clk),
+        .reset(reset),
+        .d    (RFData2),
+        .q    (DecReg_RFData2)
+    );
+
+    register U_ExeReg_RFRD2 (
+        .clk  (clk),
+        .reset(reset),
+        .d    (DecReg_RFData2),
+        .q    (ExeReg_RFData2)
+    );
+
     mux_2x1 U_ALUSrcMux (
         .sel(aluSrcMuxSel),
-        .x0 (RFData2),
-        .x1 (immExt),
+        .x0 (DecReg_RFData2),
+        .x1 (DecReg_ImmExt),
         .y  (aluSrcMuxOut)
+    );
+
+    register U_MemAccReg_dataRData (
+        .clk  (clk),
+        .reset(reset),
+        .d    (dataRData),
+        .q    (MemAccReg_dataRData)
     );
 
     mux_5x1 U_RFWDSrcMux (
         .sel(RFWDSrcMuxSel),
         .x0 (aluResult),
-        .x1 (dataRData),
-        .x2 (immExt),
+        .x1 (MemAccReg_dataRData),
+        .x2 (DecReg_ImmExt),
         .x3 (PC_Imm_AdderResult),
         .x4 (PC_4_AdderResult),
         .y  (RFWDSrcMuxOut)
@@ -62,15 +95,29 @@ module DataPath (
 
     alu U_ALU (
         .aluControl(aluControl),
-        .a(RFData1),
+        .a(DecReg_RFData1),
         .b(aluSrcMuxOut),
         .btaken(btaken),
         .result(aluResult)
     );
 
+    register U_ExeReg_ALU (
+        .clk  (clk),
+        .reset(reset),
+        .d    (aluResult),
+        .q    (ExeReg_aluResult)
+    );
+
     extend U_ImmExtend (
         .instrCode(instrCode),
         .immExt(immExt)
+    );
+
+    register U_DecReg_ImmExtend (
+        .clk  (clk),
+        .reset(reset),
+        .d    (immExt),
+        .q    (DecReg_ImmExt)
     );
 
     mux_2x1 U_PC_Imm_Adder_SrcMux (
@@ -81,7 +128,7 @@ module DataPath (
     );
 
     adder U_PC_Imm_Adder (
-        .a(immExt),
+        .a(DecReg_ImmExt),
         .b(PC_Imm_Adder_SrcMuxOut),
         .y(PC_Imm_AdderResult)
     );
@@ -100,11 +147,19 @@ module DataPath (
         .y  (PCSrcMuxOut)
     );
 
-    register U_PC (
-        .clk(clk),
+    register U_ExeReg_PCSrcMux (
+        .clk  (clk),
         .reset(reset),
-        .d(PCSrcMuxOut),
-        .q(PCOutData)
+        .d    (PCSrcMuxOut),
+        .q    (ExeReg_PCSrcMuxOut)
+    );
+
+    registerEn U_PC (
+        .clk  (clk),
+        .reset(reset),
+        .en   (PCEn),
+        .d    (ExeReg_PCSrcMuxOut),
+        .q    (PCOutData)
     );
 
 
@@ -157,6 +212,21 @@ module register (
     always_ff @(posedge clk, posedge reset) begin
         if (reset) q <= 0;
         else q <= d;
+    end
+endmodule
+
+module registerEn (
+    input  logic        clk,
+    input  logic        reset,
+    input  logic        en,
+    input  logic [31:0] d,
+    output logic [31:0] q
+);
+    always_ff @(posedge clk, posedge reset) begin
+        if (reset) q <= 0;
+        else begin
+            if (en) q <= d;
+        end
     end
 endmodule
 
