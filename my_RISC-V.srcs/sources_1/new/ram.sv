c39f7b99eb13fae3353f0d7a8dd6248024543ae3
pumpk <cong2738@gmail.com>
2025-04-17 09:12:14 +0900

    feat: 직접 입력이 아닌 컴파일 된inscode뭉치 넣을 수 있게 변경

--- my_RISC-V.srcs/sources_1/new/ram.sv
+++ my_RISC-V.srcs/sources_1/new/ram.sv
@@ -7,7 +7,7 @@ module ram (
     input  logic [31:0] wData,
     output logic [31:0] rData
 );
-    logic [31:0] mem[0:9];
+    logic [31:0] mem[0:63];
 
     always_ff @(posedge clk) begin : save
         if (we) 
