b9b6798ced1d49710a05bdac83d63913a2c8df48
pumpk <cong2738@gmail.com>
2025-04-17 10:01:07 +0900

    일단 하던거 중지 하던거: 각 전처리 단어들 시뮬레이션에서 좀 더 쉽게 보기 위해 enum으로 만들기

--- my_RISC-V.srcs/sources_1/new/defines.sv
+++ my_RISC-V.srcs/sources_1/new/defines.sv
@@ -1,3 +1,40 @@
+// //OPCOED
+// typedef enum {
+//     R_Type  = 7'b0110011,
+//     L_Type  = 7'b0000011,
+//     I_Type  = 7'b0010011,
+//     S_Type  = 7'b0100011,
+//     B_Type  = 7'b1100011,
+//     LU_Type = 7'b0110111,
+//     AU_Type = 7'b0010111,
+//     J_Type  = 7'b1101111,
+//     JL_Type = 7'b1100111
+// } op_type;
+
+// //func
+// typedef enum {
+//     ADD  = 4'b0_000,
+//     SUB  = 4'b1_000,
+//     SLL  = 4'b0_001,
+//     SRL  = 4'b0_101,
+//     SRA  = 4'b1_101,
+//     SLT  = 4'b0_010,
+//     SLTU = 4'b0_011,
+//     XOR  = 4'b0_100,
+//     OR   = 4'b0_110,
+//     AND  = 4'b0_111
+// } cal_type;
+
+// //B_Type func
+// typedef enum {
+//     BEQ  = 4'b000,
+//     BNE  = 4'b001,
+//     BLT  = 4'b100,
+//     BGE  = 4'b101,
+//     BLTU = 4'b110,
+//     BGEU = 4'b111
+// } name;
+
 //OPCOED
 `define R_Type 7'b0110011
 `define L_Type 7'b0000011
@@ -28,3 +65,4 @@
 `define BGE 4'b101
 `define BLTU 4'b110
 `define BGEU 4'b111
+
