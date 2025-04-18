98920ea84128307b357f8c102a508a06ae61089c
pumpk <cong2738@gmail.com>
2025-04-18 15:54:51 +0900

    feat: APB_Slave 모듈 추가 및 기본 동작 구현

---
+++ my_RISC-V.srcs/sources_1/new/APB_Slave.sv
@@ -0,0 +1,47 @@
+`timescale 1ns / 1ps
+
+module APB_Slave (
+    // Global Signal                (APB_MS - APB_SL)
+    input  logic        pclk,
+    input  logic        preset,
+    // APB Interface Signal
+    input  logic [ 3:0] PADDR,
+    input  logic        PWRITE,
+    input  logic [31:0] PWDATA,
+    input  logic        PENABLE,
+    input  logic        PSEL,
+    output logic [31:0] PRDATA,
+    output logic        PREADY
+);
+    logic [31:0] slv_reg0, slv_reg1, slv_reg2, slv_reg3;
+
+    always_ff @(posedge pclk, posedge preset) begin : state_logic
+        if (preset) begin
+            slv_reg0 <= 0;
+            slv_reg1 <= 0;
+            slv_reg2 <= 0;
+            slv_reg3 <= 0;
+        end else begin
+            PREADY   <= 0;
+            if (PSEL && PENABLE) begin
+                PREADY <= 1;
+                if (PWRITE) begin
+                    case (PADDR[3:2])
+                        2'd0: slv_reg0 <= PWDATA;
+                        2'd1: slv_reg1 <= PWDATA;
+                        2'd2: slv_reg2 <= PWDATA;
+                        2'd3: slv_reg3 <= PWDATA;
+                    endcase
+                end else begin
+                    PRDATA <= 32'bx;
+                    case (PADDR[3:2])
+                        2'd0: PRDATA <= slv_reg0;
+                        2'd1: PRDATA <= slv_reg1;
+                        2'd2: PRDATA <= slv_reg2;
+                        2'd3: PRDATA <= slv_reg3;
+                    endcase
+                end
+            end
+        end
+    end
+endmodule
